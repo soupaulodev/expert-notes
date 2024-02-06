30bbc783daa9d86be65a20f8d5f805836feccdc0
Paulo Marques <78260151+paulomarquesdev@users.noreply.github.com>
2024-02-06 17:51:14 +0000

    add text editor on new-note-card

--- src/components/new-note-card.tsx
+++ src/components/new-note-card.tsx
@@ -1,7 +1,20 @@
 import * as Dialog from "@radix-ui/react-dialog";
 import { X } from 'lucide-react';
+import { ChangeEvent, useState } from "react";
 
 export function NewNoteCard () {
+    const [shouldShowOnboarding, setShouldShowOnboarding] = useState(true);
+
+    function handleStartEditor () {
+      setShouldShowOnboarding(false)
+    }
+    
+    function handleContentChanged (event: ChangeEvent<HTMLTextAreaElement>) {
+      if (event.target.value === '') {
+        setShouldShowOnboarding(true)
+      }
+    }
+
     return (
       <Dialog.Root>
       <Dialog.Trigger className='rounded-md text-left flex flex-col bg-slate-800 p-5 gap-3 overflow-hidden relative outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400'>
@@ -23,7 +36,15 @@ export function NewNoteCard () {
               <div className='flex flex-1 flex-col gap-3 p-5'>
                   <span className='text-sm font-medium text-slate-300'>Adicionar nota</span>
 
-                  <p className='text-sm leading-6 text-slate-400'>Comece <button type='button' className='font-medium text-lime-400 hover:underline'>gravando uma nota</button> em áudio ou se preferir <button type='button' className='font-medium text-lime-400 hover:underline'>utilize apenas texto</button>.</p>
+                  {shouldShowOnboarding ? (
+                    <p className='text-sm leading-6 text-slate-400'>Comece <button type='button' className='font-medium text-lime-400 hover:underline'>gravando uma nota</button> em áudio ou se preferir <button type='button' onClick={handleStartEditor} className='font-medium text-lime-400 hover:underline'>utilize apenas texto</button>.</p>
+                  ) : (
+                    <textarea
+                      autoFocus
+                      className="text-sm leading-6 text-slate-400 bg-transparent resize-none flex-1 outline-none"
+                      onChange={handleContentChanged}
+                    />
+                  )}
               </div>
 
               <button type='button' className='w-full bg-lime-400 py-4 text-center text-sm text-lime-950 outline-none font-medium group hover:bg-lime-500'>Salvar nota</button>
