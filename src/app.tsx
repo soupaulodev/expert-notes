f006e4ae31dd5a211a9a407621f9724079268929
paulomarquesdev <paulomarques.rs1@gmail.com>
2024-02-08 01:47:49 -0300

    add note delete and responsive screen ui

--- src/app.tsx
+++ src/app.tsx
@@ -35,6 +35,20 @@ export function App() {
     localStorage.setItem('notes', JSON.stringify(notesArray))
   }
 
+  function onNoteDeleted (id: string) {
+    const notesArray = notes.filter(note => {
+      return note.id !== id
+    })
+
+    setNotes(notesArray)
+    localStorage.setItem('notes', JSON.stringify(notesArray))
+  }
+
+  function onAllNoteDeleted () {
+    setNotes([])
+    localStorage.setItem('notes', JSON.stringify([]))
+  }
+
   function handleSearch (event: ChangeEvent<HTMLInputElement>) {
     const query = event.target.value
 
@@ -45,12 +59,8 @@ export function App() {
     ? notes.filter(note => note.content.toLocaleLowerCase().includes(search.toLocaleLowerCase()))
     : notes
 
-  function clearAllNotes () {
-    setNotes([])
-  }
-
   return (
-    <div className="mx-auto max-w-6xl my-12 space-y-6">
+    <div className="mx-auto max-w-6xl my-12 space-y-6 px-5">
       <img src={logo} alt='NLW Expert'/>
       <div className='flex'>
         <form className="w-full" action="">
@@ -61,19 +71,19 @@ export function App() {
             onChange={handleSearch}
           />
         </form>
-        <button type="button" onClick={clearAllNotes} className='flex flex-row items-center gap-2 text-slate-400 hover:text-red-400'>
+        <button type="button" onClick={onAllNoteDeleted} className='flex flex-row items-center gap-2 text-slate-400 hover:text-red-400'>
           <Eraser className='size-5'/>
           <p className='text-sm text-nowrap'>Apagar tudo</p>
         </button>
 
       </div>
-      <div className='h-px bg-slate-400' />
+      <div className='h-px bg-slate-700' />
       
-      <div className='grid grid-cols-3 gap-6 auto-rows-[250px]'>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 auto-rows-[250px]'>
         <NewNoteCard onNoteCreated={onNoteCreated} />
         
         {filteredNotes.map(note => {
-          return <NoteCard key={note.id} note={note} />
+          return <NoteCard key={note.id} note={note} onNoteDeleted={onNoteDeleted} />
         })}
       </div>
     </div>
