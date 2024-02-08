f4ff276ba4c0b2395f3978cc59bda2d854748763
paulomarquesdev <paulomarques.rs1@gmail.com>
2024-02-07 23:16:11 -0300

    add features: delete all, save new notes, close editor, validate form, reflect handleChangedContent on att value of textarea

--- src/app.tsx
+++ src/app.tsx
@@ -1,39 +1,57 @@
+import { useState } from 'react';
 import logo from './assets/logo-nlw-expert.svg'
 import { NewNoteCard } from './components/new-note-card';
 import { NoteCard } from './components/note-card';
+import { Eraser } from 'lucide-react';
 
-const note = {
-  date: new Date(),
-  content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. In optio hic obcaecati? Veniam laboriosam fugiat quisquam culpa eum recusandae vero itaque. Aspernatur autem ratione suscipit eveniet. Explicabo temporibus provident sapiente?'
+interface Note {
+  id: string,
+  date: Date,
+  content: string
 }
 
 export function App() {
+  const [notes, setNotes] = useState<Note[]>([])
+
+  function onNoteCreated(content: string) {
+    const newNote = {
+      id: crypto.randomUUID(),
+      date: new Date(),
+      content,
+    }
+
+    setNotes([newNote, ...notes])
+  }
+
+  function clearNotes () {
+    setNotes([])
+  }
+
   return (
     <div className="mx-auto max-w-6xl my-12 space-y-6">
       <img src={logo} alt='NLW Expert'/>
-      <form className="w-full" action="">
-        <input 
-          type="text" 
-          placeholder='Busque em suas notas'
-          className='w-full bg-transparent text-3xl font-semibold -tracking-tight outline-none placeholder:text-slate-500'
-        />
-      </form>
+      <div className='flex'>
+        <form className="w-full" action="">
+          <input 
+            type="text" 
+            placeholder='Busque em suas notas'
+            className='w-full bg-transparent text-3xl font-semibold -tracking-tight outline-none placeholder:text-slate-500'
+          />
+        </form>
+        <button type="button" onClick={clearNotes} className='flex flex-row items-center gap-2 text-slate-400 hover:text-red-400'>
+          <Eraser className='size-5'/>
+          <p className='text-sm text-nowrap'>Apagar tudo</p>
+        </button>
+
+      </div>
       <div className='h-px bg-slate-400' />
       
       <div className='grid grid-cols-3 gap-6 auto-rows-[250px]'>
-        <NewNoteCard />
+        <NewNoteCard onNoteCreated={onNoteCreated} />
         
-        <NoteCard note={note} />
-        
-        <NoteCard note={note} />
-        
-        <NoteCard note={note} />
-        
-        <NoteCard note={note} />
-        
-        <NoteCard note={note} />
-        
-
+        {notes.map(note => {
+          return <NoteCard key={note.id} note={note} />
+        })}
       </div>
     </div>
   )
--- src/components/new-note-card.tsx
+++ src/components/new-note-card.tsx
@@ -3,9 +3,14 @@ import { X } from 'lucide-react';
 import { ChangeEvent, FormEvent, useState } from "react";
 import { toast } from 'sonner'
 
-export function NewNoteCard () {
+interface NewNoteCardProps {
+  onNoteCreated: (content: string) => void
+}
+
+export function NewNoteCard ({ onNoteCreated }: NewNoteCardProps) {
     const [shouldShowOnboarding, setShouldShowOnboarding] = useState(true);
     const [content, setContent] = useState('');
+    const [buttonDisable, setButtonDisable] = useState(true);
 
     function handleStartEditor () {
       setShouldShowOnboarding(false)
@@ -14,30 +19,40 @@ export function NewNoteCard () {
     function handleContentChanged (event: ChangeEvent<HTMLTextAreaElement>) {
       if (event.target.value === '') {
         setShouldShowOnboarding(true)
+        setButtonDisable(true)
+      } else {
+        setButtonDisable(false)
+        setContent(event.target.value);
       }
-      setContent(event.target.value);
     }
 
     function handleSaveNote (event: FormEvent) {
       event.preventDefault()
-      content!=='' ? toast.success('Nota criada com sucesso!') : toast.error('Voce não pode adicionar uma nota vazia.')
+      if (content!=='') {
+        onNoteCreated(content)
+        toast.success('Nota criada com sucesso!')
+        setContent('')
+        setShouldShowOnboarding(true)
+        setButtonDisable(true)
+      } else {
+        toast.error('Voce não pode adicionar uma nota vazia.')
+      }
     }
 
     return (
       <Dialog.Root>
-      <Dialog.Trigger className='rounded-md text-left flex flex-col bg-slate-800 p-5 gap-3 overflow-hidden relative outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400'>
+      <Dialog.Trigger className='rounded-md text-left flex flex-col bg-slate-700 p-5 gap-3 overflow-hidden relative outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400'>
 
           <span className='text-sm font-medium text-slate-300 text-transform: capitalize'>Adicionar nota</span>
 
           <p className='text-sm leading-6 text-slate-400'>Grave uma nota em áudio que será convertido para texto automaticamente.</p>
-
-          <div className='absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none' />
+          
       </Dialog.Trigger>
       <Dialog.Portal>
           <Dialog.Overlay className='inset-0 fixed bg-black/60'></Dialog.Overlay>
           <Dialog.Content className='overflow-hidden fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] w-full h-[60vh] bg-slate-700 rounded-md flex flex-col ontline-none'>
               
-              <Dialog.Close className='absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover: text-slate-100'>
+              <Dialog.Close className='absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 rounded-bl-md hover: text-slate-100'>
                   <X className='size-5'/>
               </Dialog.Close>
               
@@ -53,11 +68,12 @@ export function NewNoteCard () {
                         autoFocus
                         className="text-sm leading-6 text-slate-400 bg-transparent resize-none flex-1 outline-none"
                         onChange={handleContentChanged}
+                        value={content}
                       />
                     )}
                 </div>
 
-                <button type='submit' className='w-full bg-lime-400 py-4 text-center text-sm text-lime-950 outline-none font-medium group hover:bg-lime-500'>Salvar nota</button>
+                <button disabled={buttonDisable} type='submit' className='w-full bg-lime-400 py-4 text-center text-sm text-lime-950 outline-none font-medium group hover:bg-lime-500 disabled:bg-slate-800 disabled:text-slate-500'>Salvar nota</button>
               </form>
           </Dialog.Content>
       </Dialog.Portal>
