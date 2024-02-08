9f8986a45c7c49c8b86606b2d0c3e24038b9489b
paulomarquesdev <paulomarques.rs1@gmail.com>
2024-02-08 01:59:04 -0300

    fix bug

--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -28,7 +28,7 @@ export function NoteCard({ note, onNoteDeleted }: NoteCardProps) {
                 <Dialog.Overlay className='inset-0 fixed bg-black/60'></Dialog.Overlay>
                 <Dialog.Content className='overflow-hidden fixed inset-0 md:inset-auto md:left-1/2 md:top-1/2 md:-translate-x-1/2 md:-translate-y-1/2 md:max-w-[640px] w-full md:h-[60vh] bg-slate-700 md:rounded-md flex flex-col ontline-none'>
                     
-                    <Dialog.Close className='absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover: text-slate-100'>
+                    <Dialog.Close className='absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100'>
                         <X className='size-5'/>
                     </Dialog.Close>
                     
